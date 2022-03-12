8cdaf5df652c4bff29650d484137a4d3847c9f1b
JagannathSahu <jagannath.sahu@in.abb.com>
2022-03-12 13:25:51 +0530

    Code clean

--- TestGIT/Program.cs
+++ TestGIT/Program.cs
@@ -7,27 +7,7 @@ namespace TestGIT
         static void Main(string[] args)
         {
 
-            Test.Create("English", "13/April/2022");
-            Console.WriteLine("123654654 added one console file");
-            
-            Console.WriteLine("Testing");
-
-            Employee.Name("ABC");
-
-            Console.WriteLine("Development branch check in");
-            Console.WriteLine("Here also changed");
-
-            Console.WriteLine("Master Branch Changes");
-
- 
-            Console.WriteLine("Master Branch Changes"); 
-            Console.WriteLine("Here going change");
-            Student.Name("hari");
-            Console.WriteLine("Here also changing and changed");
-            Abc123.MEthod();
-            Console.WriteLine("This code will merge in master branch");
-            Console.WriteLine("Added some file in abc123");
-            Console.WriteLine("Added some file in abc12 files 3 at 12march113pm");
+            Console.WriteLine("Clean the code");
             Console.ReadLine(); 
         }
     }
