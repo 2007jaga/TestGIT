0f12cad0af0de430588d2d0f627bd51cdc6827f6
JagannathSahu <jagannath.sahu@in.abb.com>
2022-03-12 16:16:19 +0530

    black color added

--- TestGIT/Student.cs
+++ TestGIT/Student.cs
@@ -8,7 +8,7 @@ namespace TestGIT
     {
         public static void Name(string Name)
         {
-            Console.WriteLine( "testing changing something"  +Name.ToString());
+            Console.WriteLine( "Black");
         }
     }
 }
