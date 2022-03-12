c28572ca863404cd377d77c8218575a95fdfddbc
JagannathSahu <jagannath.sahu@in.abb.com>
2022-03-12 16:37:30 +0530

    added

--- TestGIT/Student.cs
+++ TestGIT/Student.cs
@@ -8,7 +8,7 @@ namespace TestGIT
     {
         public static void Name(string Name)
         {
-            Console.WriteLine( "red");
+            Console.WriteLine( "testing");
         }
     }
 }
